5f081d3979a5fc2ab02e4b3e5a3bd270ed43cc92
Andrew Yudi <andrewyudi@andoryunoMacBook-Air.local>
2022-05-29 16:26:23 +0900

    ターミナルから初

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
+import { useRef, useState } from "react";
 import './App.css';
 
 function App() {
+  const [val, setVal] = useState('ok');
+
+  const handleChange = e => setVal(e.target.value);
+
+  const text = useRef(null)
+
+  const handleSubmit = () => {
+    alert(val)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <h1>トイレ状態管理</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>綺麗でしたか？</label>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="ok"
+                  onChange={handleChange}
+                  checked={val === "ok"}
+                />
+                ok</label>
+              <label>
+                <input
+                  type="radio"
+                  value="none"
+                  onChange={handleChange}
+                  checked={val === 'none'}
+                   />
+                none</label>
+            </div>
+            <p>選択された値は{val}</p>
+          </div>
+          <button className="submitButton" onClick={handleSubmit} type="submit">送信してね</button>
+        </div>
+      </form>
     </div>
   );
 }
